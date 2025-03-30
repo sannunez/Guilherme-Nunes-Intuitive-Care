5e252c4e597b0adbb5db7adda979586a5caeaf9b
unknown <sftwr.gsantosnunes@gmail.com>
2025-03-30 19:46:59 -0300

    Teste de Web Scrapping(melhorado) & Teste de Transformação de Dados

--- Teste_De_WebScraping/requisicao_gov.py
+++ Teste_De_WebScraping/requisicao_gov.py
@@ -1,25 +1,31 @@
+# 1. TESTE DE WEB SCRAPING
+
 # lib's imports
 import requests
 from bs4 import BeautifulSoup
 import os
 from zipfile import ZipFile, ZIP_DEFLATED
 
+# 1.1 | Acesso ao Site:
 def request_e_parse(url):
     requisicao = requests.get(url)
     if requisicao.status_code == 200:
         # Parsing para HTML
+        print('Status Requisição | Bem sucedida.\n')
         return BeautifulSoup(requisicao.text, 'html.parser')
     else:
         print('''Status Requisição: Falhou. 
               Verifique o URL solicitado''')
         return None
 
-def download_zip_anexos(soup):
+# 1.2 | Download dos Anexos I e II em formato PDF:
+def download_anexos(soup, nome_pasta_anexos):
     links_anexos = soup.find_all('a', class_='internal-link')
 
     # Diretorio de anexos
-    os.makedirs('Anexos', exist_ok=True) 
-
+    pasta_path = f'Teste_de_WebScraping/{nome_pasta_anexos}'
+    os.makedirs(pasta_path, exist_ok=True) 
+    
     # Iterador (Modifica nomes para: Anexo 1, Anexo 2, ..., Anexo n.)
     i = 0
 
@@ -29,16 +35,24 @@ def download_zip_anexos(soup):
             if '.pdf' in a['href']:
                 anexo_link = requests.get(a['href'])
                 i += 1 # Iterador incrementando
-                with open(f'Anexos/Anexo{i}.pdf', 'wb') as anexo:
-                  anexo.write(anexo_link.content)       
+                with open(f'{pasta_path}/Anexo{i}.pdf', 'wb') as anexo:
+                  anexo.write(anexo_link.content)
+                  print(f'Arquivo PDF: Anexo{i} | Baixado com sucesso.')
 
-    # Zipando anexos
-    with ZipFile('Anexos.zip', 'w', ZIP_DEFLATED) as arquivo_zip:
-        for raiz, _, arquivos in os.walk('Anexos'):
+    return pasta_path 
+
+
+def zipar(zip_nome, pasta_path):
+
+    with ZipFile(f'Teste_de_WebScraping/{zip_nome}.zip', 'w', ZIP_DEFLATED) as arquivo_zip:
+        for raiz, _, arquivos in os.walk(pasta_path):
             for arquivo in arquivos:
                 caminho_completo = os.path.join(raiz, arquivo)
-                caminho_no_zip = os.path.relpath(caminho_completo, 'Anexos')
+                caminho_no_zip = os.path.relpath(caminho_completo, pasta_path)
                 arquivo_zip.write(caminho_completo, caminho_no_zip)
-    
-                
-download_zip_anexos(request_e_parse("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"))
+    print(f'Arquivo Zip: {zip_nome} | Criado com sucesso.')
+
+# EXECUÇÃO
+requisicao = request_e_parse("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos")
+anexos = download_anexos(requisicao, 'Anexos')
+zipar('Meus_Anexos', anexos)
---
+++ Teste_de_Transformacao_de_Dados/rol_de_procedimentos.py
@@ -0,0 +1,52 @@
+# 2 Teste de Transformação de Dados
+
+# lib's imports
+import pandas as pd
+import tabula as tb
+import os
+from zipfile import ZipFile, ZIP_DEFLATED
+from tkinter import filedialog
+
+# 2.1 | Extraindo os dados da Tabela Rol de Procedimentos:
+# 2.2 | Salvando dados em tabela estruturada:
+def criar_dataframe(pdf_caminho, csv_nome, pagina_de_inicio, pagina_de_fim):
+    tb.convert_into(f'{pdf_caminho}', f'Teste_de_Transformacao_de_Dados/{csv_nome}.csv', pages = f'{pagina_de_inicio}-{pagina_de_fim}')
+    df = f'Teste_de_Transformacao_de_Dados/{csv_nome}.csv'
+    return df
+
+
+# 2.3 | Compactando csv em arquivo denonimado "Teste_{seu_nome}.zip" 
+def zipar(seu_nome, df):
+    with ZipFile(f'Teste_de_Transformacao_de_Dados/Teste_{seu_nome}.zip', 'w', ZIP_DEFLATED) as arquivo_zip:
+        df_path_zip = os.path.relpath(df,'Teste_de_Transformacao_de_Dados')
+        arquivo_zip.write(df, df_path_zip)
+    print(f'Arquivo ZIP: Teste_{seu_nome} | Criado com sucesso.')
+
+
+# 2.4 | Substituindo abreviações
+def substituir_abreviacao(dataframe, abreviacao, substituto):
+
+    df = pd.read_csv(dataframe)
+
+    if abreviacao in df.columns:
+        coluna = df.columns.get_loc(abreviacao)
+
+        for linha in range(len(df)):
+            if pd.notnull(df.iloc[linha, coluna]):
+                df.loc[linha, abreviacao] = substituto
+        print(f'Processamento concluido | Abreviação {abreviacao} substituida por {substituto}')
+
+        df.to_csv(dataframe, index=False)
+    else:
+        print(f'''Erro com a abreviação: {abreviacao}. 
+Verifique: 1 | Se a coluna existe. 
+           2 | Se houve um erro de digitação.''')
+
+
+# EXECUÇÃO
+pdf_caminho = str(os.path.abspath(filedialog.askopenfilename()))
+dataframe = criar_dataframe(pdf_caminho, 'Anexo_1', 3, 181)
+zipar('Guilherme Nunes', dataframe)
+substituir_abreviacao(dataframe, 'OD', 'Seg. Odontológica')
+substituir_abreviacao(dataframe, 'AMB', 'Seg. Ambulatórial')
+
